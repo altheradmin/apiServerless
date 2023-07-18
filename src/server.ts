28c1f29acd2d8be461b195bf3636a1a73c4deabb
altheradmin <91630778+altheradmin@users.noreply.github.com>
2023-07-18 19:45:41 -0300

    Update server.ts
    updatedmaster

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 app.use(cors());
-
+//mcrl
 app.use(express.json());
 
 const { MongoClient } = require('mongodb');
